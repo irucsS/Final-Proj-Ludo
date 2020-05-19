4985b0fd02fb9b5504a8c85aa44d5956cf708827
June <59287664+bronzy295843@users.noreply.github.com>
2020-05-19 21:29:01 +0500

    the button.h class class to assist the dice class.

---
+++ Button.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "graphics.h"
+#include "Point.h"
+class Button
+{
+protected:
+	int value;
+public: 
+	Point index;
+	 Button();
+	 Button(Point i, int v = 0);
+	 void ChangeValue(int v); // Change Value of the button for every turn
+	 int GetValue(); // returns the value stored in the button
+	 void ChangeIndex(Point i);
+	 void Draw(Point S); // Draw the button
+	 void EraseValue(Point S); // Erase the number drawn in it
+	 void DrawValue(Point S); // Draw the value in the button
+	 bool Is_Clicked(Point S);
+
+	 ~Button() {}
+	
+};
