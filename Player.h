f1a8b60bcfac970a1a2a9e69c7a9922367208f15
dza31 <64732420+dza31@users.noreply.github.com>
2020-05-19 21:21:12 +0500

    Updated Player.h

---
+++ Player.h
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+//#include"Board.h"
+using namespace std;
+#pragma once
+
+class Piece;
+class Board;
+class Player
+{
+	friend class Team;
+	friend class LudoGame;
+	friend class Board;
+protected:
+	string Name;
+	Board* Home;
+	int Clr;
+	Piece* Pcs; int nops;
+public:
+	Player(const string& N, int C, int NOP,int Tn);
+	Board& operator[](int i);
+	int CurrIndex(Piece* FP);		//this returns where the piece is lying on the home array 
+	int PiecesLeft();	//return how many pieces of the player has left 
+	int GetColour()
+	{
+		return Clr;
+	}
+	bool HWon();
+	void RemovePiece(int i);
+	void Home4(int C);
+	void Home6(int C);
+};
